08fdfb0dad6c42c575ec706492da96162d6e12c5
marknhenry <marknhenry>
2022-04-19 17:09:55 +0000

    2 new lines

--- databricks_implementation/01-adb-test.py
+++ databricks_implementation/01-adb-test.py
@@ -1,2 +1,11 @@
 # Databricks notebook source
 print('From Databricks with love!')
+
+# COMMAND ----------
+
+print('This is the second brick, so to speak')
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Another brick, this time as an `md`
